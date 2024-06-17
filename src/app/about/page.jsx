5e9322a574e7d0784b7255353a9684f3cad9bfc9
Rohith-Manjunath <rohithmanjunath20@gmail.com>
2024-06-17 18:17:59 +0530

    Add about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,39 @@
+const About = () => {
+  return (
+    <div className="px-6 max-w-7xl mx-auto">
+      <h2 className="text-2xl sm:text-3xl my-4 text-start text-amber-400 font-semibold md:text-4xl ">
+        About
+      </h2>
+      <p className="text-[15px] sm:text-[16px] md:text-[17px]">
+        Our movie database website offers a comprehensive platform for movie
+        enthusiasts, providing a rich repository of film information spanning
+        across genres, eras, and regions. Users can explore detailed profiles of
+        their favorite movies, complete with cast and crew information, plot
+        summaries, release dates, and user-generated ratings and reviews. The
+        intuitive search functionality allows for quick access to any film
+        title, making it easy for cinephiles to discover new movies or revisit
+        classics.
+        <br />
+        <br />
+        In addition to movie information, our platform features extensive actor
+        and director profiles, showcasing their careers and contributions to the
+        film industry. Users can delve into the filmography of any actor or
+        director, view their most notable works, and track their career
+        progression over time. This makes our website an invaluable resource for
+        anyone interested in the people behind the movies, from legendary icons
+        to emerging talents.
+        <br />
+        <br />
+        Our movie database also fosters a vibrant community of film lovers who
+        can share their thoughts and opinions through reviews and ratings. This
+        interactive element not only helps other users make informed viewing
+        choices but also enhances the overall user experience by creating a
+        space for discussion and engagement. Whether you’re a casual viewer or a
+        dedicated cinephile, our platform provides all the tools you need to
+        dive deep into the world of cinema.
+      </p>
+    </div>
+  );
+};
+
+export default About;
